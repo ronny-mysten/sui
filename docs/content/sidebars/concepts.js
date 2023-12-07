b22272b5baa6f995b66dba8bb131da40a562d0a2
Ronny Roland <ronmilton@mystenlabs.com>
2023-12-07 14:00:24 -0700

    Content from Todd

--- docs/content/sidebars/concepts.js
+++ docs/content/sidebars/concepts.js
@@ -113,7 +113,32 @@ const concepts = [
 					id: 'concepts/transactions',
 				},
 				items: [
-					'concepts/transactions/prog-txn-blocks',
+					{
+						type: 'category',
+						label: 'Programmable Transaction Blocks',
+						link: {
+							type: 'doc',
+							id: 'concepts/transactions/prog-txn-blocks',
+						},
+						items: [
+							'concepts/transactions/input-types',
+							'concepts/transactions/result-and-nested',
+							{
+								type: 'category',
+								label: 'Transaction Types',
+								link: {
+									type: 'doc',
+									id: 'concepts/transactions/transaction-types',
+								},
+								items: [
+									'concepts/transactions/transaction-types/move-call',
+									'concepts/transactions/transaction-types/transfer-object',
+									'concepts/transactions/transaction-types/split-coin',
+									'concepts/transactions/transaction-types/merge-coin',
+								],
+							},
+						],
+					},
 					'concepts/transactions/sponsored-transactions',
 					'concepts/transactions/gas-smashing',
 				],
@@ -168,18 +193,8 @@ const concepts = [
 		},
 		items: [
 			'concepts/sui-architecture/high-level',
-			{
-				type: 'category',
-				label: 'Lifetime of a Transaction',
-				link: {
-					type: 'doc',
-					id: 'concepts/transactions/transaction-lifecycle',
-				},
-				items: ['concepts/transactions/transaction-things'],
-			},
-			'concepts/sui-architecture/certification-overview',
+			'concepts/transactions/transaction-lifecycle',
 			'concepts/sui-architecture/consensus',
-			'concepts/sui-architecture/confirmation',
 			'concepts/sui-architecture/indexer-functions',
 			'concepts/sui-architecture/epochs',
 			'concepts/sui-architecture/protocol-upgrades',
