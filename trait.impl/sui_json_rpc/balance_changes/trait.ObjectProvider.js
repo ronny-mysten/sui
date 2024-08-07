a3e082b9deea29f11b2631d1fdcc2c6792dcba00
ronny-mysten <ronny-mysten@users.noreply.github.com>
2024-08-07 08:55:35 +0000

    deploy: 98603adc68a6363b5dc6ee2fed07025a6282dd3e

---
+++ trait.impl/sui_json_rpc/balance_changes/trait.ObjectProvider.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"sui_indexer":[["impl ObjectProvider for <a class=\"struct\" href=\"sui_indexer/handlers/tx_processor/struct.TxChangesProcessor.html\" title=\"struct sui_indexer::handlers::tx_processor::TxChangesProcessor\">TxChangesProcessor</a>"]],
+"sui_json_rpc":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/sui_json_rpc/trait.SuiRpcModule.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"sui_indexer":[["impl&lt;T: R2D2Connection&gt; SuiRpcModule for <a class=\"struct\" href=\"sui_indexer/apis/governance_api/struct.GovernanceReadApi.html\" title=\"struct sui_indexer::apis::governance_api::GovernanceReadApi\">GovernanceReadApi</a>&lt;T&gt;"]],
+"sui_json_rpc":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
